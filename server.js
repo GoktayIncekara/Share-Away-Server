808733e33365f47fdec450c3dcff494143d53da4
Mehmet Baran Nakipoğlu <mehmetbarannakipoglu@outlook.com>
2022-05-19 01:34:47 +0300

    Solved the CROS issue

--- server.js
+++ server.js
@@ -4,13 +4,23 @@ const cors = require("cors");
 
 require("dotenv").config();
 
+
+
+
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(express.json({extended:true}));
+app.use(express.json({ extended: true }));
+// this wil allow http request from another source --> we make calls from 3000 to 5000
+// if you do not specify the origin: --> it would allow all the calls from all the outer sources which we do not want that
+// if you try to make calls from :3001 --> it would deny it
+// also --> it should be written after the use express json as above
+app.use(cors({
+    origin: "http://localhost:3000",
+}));
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const connection = mongoose.connection;
 connection.once("open", () => {
@@ -38,6 +48,6 @@ connection.once("open", () => {
 const userRouter = require('./routes/users');
 app.use('/users', userRouter);
 
-app.listen(port, function(){
-    console.log("Server started on port "+ port);
+app.listen(port, function () {
+    console.log("Server started on port " + port);
 });
